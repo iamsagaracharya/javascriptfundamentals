b8de6b0a35e6b3e337e0c5913f17bf80d2adf952
Sagar Acharya <sagaracharya1006@gmail.com>
2025-03-25 09:38:49 +0545

    new clear button added

--- CalculatedArea/calculate_Area.js
+++ CalculatedArea/calculate_Area.js
@@ -3,6 +3,17 @@ let width;
 function calculateArea() {
     let length = parseFloat(document.getElementById("length").value);
     let width = parseFloat(document.getElementById("width").value);
-    let area = length * width;
-    document.getElementById("result").innerText = `The area of the rectangle is: ${area}`;
+    if (length && width) {
+        const area = length * width;
+        document.getElementById("result").innerText = `The area of the rectangle is: ${area}`;
+        document.getElementById("clearBtn").style.display = "inline-block";
+    } else {
+        document.getElementById("result").textContent = "Please enter both values";
+    }
+}
+function clearFields() {
+    document.getElementById("length").value = "";
+    document.getElementById("width").value = "";
+    document.getElementById("result").textContent = "";
+    document.getElementById("clearBtn").style.display = "none";
 }
